8822a94729bb9babdbe1473d93c2a5ceb102152d
Alejandro Abadía <ajabadia@gmail.com>
2026-06-24 15:10:17 +0200

    Auto-commit/Release: 2026-06-24 14:55:06

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   transpilePackages: ['@ajabadia/ecosystem-widgets', '@ajabadia/styles', '@ajabadia/satellite-sdk', 'next-intl'],
 };
 
