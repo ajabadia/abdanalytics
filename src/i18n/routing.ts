59f43acc381013db16bf2117ac6355b1f8d0366e
Alejandro Abadía <ajabadia@gmail.com>
2026-06-29 14:22:37 +0200

    Auto-commit/Release: 2026-06-29 14:03:38

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,1 +1,2 @@
-export { routing, Link, redirect, usePathname, useRouter } from '@ajabadia/i18n/routing';
+export { routing } from '@ajabadia/i18n/routing';
+export { Link, redirect, usePathname, useRouter } from '@ajabadia/i18n/navigation';
